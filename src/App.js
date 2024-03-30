06a868be4e464d2f5e77504d2757403378b3816d
Emiliano Gibran Gonzalez Perez <emiliano.gonzalez@oracle.com>
2024-03-30 01:07:13 -0600

    Adjusts css and card components
    Signed-off-by: Emiliano Gibran Gonzalez Perez <emiliano.gonzalez@oracle.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
+import { useState } from 'react';
 import './App.css';
 import products from './db.json';
 
-function ProductCard({descripcion, enlace, notas, imageurl, categoria}){
-  console.log(categoria);
+function ProductCard({ descripcion, enlace, notas, imageurl, onProductSelected}) {
   return (
     <div className='ProductCard'>
-      <h2>{descripcion}</h2>
       <img src={imageurl} alt={descripcion} className="ProductImage" />
-      <p>{notas}</p>
-      <a href={enlace}>Ver producto</a>
+      <h2 className='ProductTitle'>{descripcion}</h2>
+      <p className='ProductDescription'>{notas}</p>
+      { onProductSelected && <button onClick={() => onProductSelected({ descripcion, enlace, notas, imageurl })}>Ver Más</button>}
+      <div className='CardToolbar'>
+        <a href={enlace}>Ver producto</a>
+      </div>
     </div>
   );
 }
 
+
 function ProductContainer ({products}){
+  const [productSelected, setProductSelected] = useState(null)
+  const handleProductSelected = (product) => {
+    setProductSelected(product);
+  };
+
+  const handleModalClose = () => {
+    setProductSelected(null);
+  };
   return (
     <div className='ProductContainer'>
-      {products.map((product, index) => (
-        <ProductCard key={index} {...product} />
-        ))}
+      {
+        products.map((product, index) => (
+          <ProductCard
+            key={index}
+            {...product}
+            onProductSelected={handleProductSelected}
+            />
+        ))
+      }
+
+      {console.log(`Producto seleccionado: `, productSelected) || productSelected && (
+        <div className="Modal" onClick={handleModalClose}>
+          <span className="CloseButton" onClick={handleModalClose}>&times;</span>
+          <div className="ModalContent" onClick={(e) => e.stopPropagation()}>
+            <ProductCard {...productSelected} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
